d8b9266a46a4cb87be74731de4fe6d3af73f3d2f
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-23 16:28:08 +0200

    added style with scss

--- resources/views/layout/comics.blade.php
+++ resources/views/layout/comics.blade.php
@@ -4,16 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Comics</title>
 </head>
 <body>
+    {{-- HEADER  --}}
     @include('partials.header')
+    {{--END HEADER  --}}
 
-    <div class="content">
+    {{-- MAIN  --}}
+    <main>
 
         @yield('content')
-    </div>
 
+    </main>
+    {{-- END MAIN  --}}
+
+    {{-- FOOTER  --}}
     @include('partials.footer')
+    {{-- END FOOTER  --}}
+
 </body>
 </html>
