e3a886c1c76991f3f1045327ba974a272c686262
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-23 16:17:46 +0200

    passed and printed data

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.comics')
 
 @section('content')
- <p>section</p>
+
+{{-- @dd($comics) --}}
+@foreach ( $comics as  $comic)
+
+<div class="comic-card">
+    <img src="{{$comic->image}}" alt="">
+    <p>{{$comic->title}}</p>
+    <p>{{$comic->type}}</p>
+
+</div>
+
+@endforeach
+
 @stop
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,3 @@
 <header>
-    <ul>
-        <li><a href="">Comics</a></li>
-    </ul>
+    <h1>comics</h1>
 </header>
